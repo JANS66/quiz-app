b3eeac5a0513dcddedacd80523e59c7e4a24579a
JANS66 <98973687+JANS66@users.noreply.github.com>
2026-01-02 18:00:28 +0200

    Display Current Question

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,18 @@ function App() {
   const [score, setScore] = useState(0)
   const [isFinished, setIsFinished] = useState(false)
 
+  const currentQuestion = questions[currentQuestionIndex]
+
+  const question = currentQuestion.question
+  const buttons = currentQuestion.options.map((option, index) => (<button onClick={() => chooseAnswer(option)} key={index}>{option}</button>))
+
+  const chooseAnswer = answer => setSelectedAnswer(answer)
+
   return (
-    <>
-    </>
+    <div>
+      {question}
+      {buttons}
+    </div>
   )
 }
 
